363049e6bed81fe2968d09d3956292bfbb322cbf
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-04-20 13:44:17 -0700

    Initial Commit

---
+++ SubscriptionUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SubscriptionUI
+//
+//  Created by Mohammed Alsaleh on 19/09/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SubscriptionUI/SubscriptionUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionUIApp.swift
+//  SubscriptionUI
+//
+//  Created by Mohammed Alsaleh on 19/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
